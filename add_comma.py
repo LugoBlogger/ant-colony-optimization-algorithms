2cff06e94306150207b93569b86983654f25e486
LugoBlogger <62300094+LugoBlogger@users.noreply.github.com>
2020-05-14 13:30:10 +0700

    fixed .split() argument

--- add_comma.py
+++ add_comma.py
@@ -7,7 +7,7 @@ def add_comma(input_file, output_file):
 
     xy_coor = [line.strip('\n') for line in data]
     xy_coor = [line.lstrip().rstrip() for line in xy_coor]
-    xy_coor = [line.split(' ') for line in xy_coor]
+    xy_coor = [line.split() for line in xy_coor]
     xy_coor = [[r.strip() for r in line] for line in xy_coor]
     xy_coor = [', '.join(line) for line in xy_coor]
 
